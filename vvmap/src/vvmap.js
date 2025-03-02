a7aed26dae4c176dcdd02e694782c3501eb6dfac
Sergey Kirienko <skirienko@gmail.com>
2025-03-03 00:18:32 +0200

    improvements, legend

--- vvmap/src/vvmap.js
+++ vvmap/src/vvmap.js
@@ -5,11 +5,19 @@ import '@maplibre/maplibre-gl-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
+const LEGEND = {
+  'f': {color:'#FF3333', descr: 'улицы в честь женщин'},
+  'm': {color:'#3333FF', descr: 'улицы в честь мужчин'},
+  '-': {color:'#99AA99', descr: 'нейтральные улица'},
+  '!': {color:'#CC33CC', descr: 'как бы нейтральные, но вообще-то в честь мужчин'},
+  '?': {color:'#FFDD66', descr: 'непонятно'}
+}
+
 console.log("Hi!")
 console.log(L)
 console.log(maplibregl)
 
-var map = L.map('map', {attributionControl:false}).setView([43.103, 131.905], 12);
+const map = L.map('map', {attributionControl:false}).setView([43.103, 131.905], 12);
 
 var gl = L.maplibreGL({
           // style: 'https://tiles.openfreemap.org/styles/positron'
@@ -19,22 +27,35 @@ var gl = L.maplibreGL({
 
 fetch('./streets.json').then(r => r.json()).then(data => drawStreets(data));
 
+const legend = L.control();
+
+legend.onAdd = function(map) {
+  const ul = L.DomUtil.create('ul', 'legend');
+  renderLegend(ul);
+  return ul;
+}
+legend.addTo(map);
+
 function drawStreets(data) {
     Object.keys(data).forEach(k => {
       const color = getColor(data[k]['gender'])
       data[k]['parts'].forEach(part => {
-        const kal = L.polyline(part, {color:color}).addTo(map);
+        const kal = L.polyline(part, {color:color,opacity:.75}).addTo(map);
       });
     });
 }
 
-const COLORS = {
-  'f': 'red',
-  'm': 'blue',
-  '-': 'gray',
-  '!': 'purple'
+function renderLegend(leg) {
+  if (leg) {
+    Object.values(LEGEND).forEach(item => {
+      const li = document.createElement('li');
+      li.setAttribute('style', "color:"+item.color);
+      li.appendChild(document.createTextNode(item.descr));
+      leg.appendChild(li);
+    });
+  }
 }
 
 function getColor(key) {
-  return key in COLORS ? COLORS[key] : 'yellow'
+  return key in LEGEND ? LEGEND[key].color : LEGEND['?'].color
 }
